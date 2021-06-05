a79b28ee88c3bd0ec52384298affab0c09908c97
Andy Moreno <andymoreno.ing@gmail.com>
2021-06-04 22:24:11 -0500

    Proyecto Tiempo de Paises y Ciudades

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,48 @@
+<template>
+    <app-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Dashboard
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    
+                    <select v-model="pais">
+                        <option value="VE">Venezuela</option>
+                        <option value="CO">Colombia</option>
+                    </select>
+                    <select v-model="ciudad">
+                        <option value="Caracas">Caracas</option>
+                        <option value="Bogota">Bogota</option>
+                    </select>
+
+                    {{pais}}
+                    {{ciudad}}
+
+                </div>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+    import AppLayout from '@/Layouts/AppLayout'
+
+    export default {
+        components: {
+            AppLayout,
+        },
+        data() {
+            return {
+                pais: '',
+                ciudad: ''
+            }
+        },
+        created(){
+
+        }
+    }
+</script>
